9e1b175952ba9b8dcbe48f470cd7f33e06d97b1e
Priyesh <91priyesh@gmail.com>
2019-01-10 21:21:51 -0500

    added calculations, there is an error with frequencyand .trim, will confirm with alex tomorrow and upload

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -17,43 +17,62 @@ $(document).ready(function () {
     event.preventDefault();
 
     var trainData = {
-      name: $("#name-input").val().trim(),
-      destination: $("#destination-input").val().trim(),
-      time: $("#time-input").val(),
-      // frequency: $("frequency-input").val()
-    };
-
-    // //remember to conver time to unix
-    // trainData.time = moment(trainData.time).format("X");
+      trainName: $("#t-name-input").val().trim(),
+      trainDestination: $("#t-destination-input").val().trim(),
+      firstTime: $("#first-time-input").val().trim(),
+      trainFrequency: $("t-frequency-input").val().trim()
+    }
+    
     //push data to firebase
     database.ref().push(trainData);
 
     //clear value in form input tags on page
-    $("#name-input").val("");
-    $("#destination-input").val("");
-    $("#time-input").val("");
-    $("#frequency-input").val("");
+    $("#t-name-input").val("");
+    $("#t-destination-input").val("");
+    $("#first-time-input").val("");
+    $("#t-frequency-input").val("");
   });
 
 
 
   //child_added event listener for firebase to send new information everytime a new train is added and when page loads
-  database.ref().on("child_added", function (childSnapshot) {
+  database.ref().on("child_added", function(childSnapshot) {
     console.log(childSnapshot.val());
-    var name = childSnapshot.val().name;
-    var destination = childSnapshot.val().destination;
-    var time = childSnapshot.val().time;
-    var frequency = childSnapshot.val().frequency;
+    var trainName = childSnapshot.val().trainName;
+    var trainDestination = childSnapshot.val().trainDestination;
+    var firstTime = childSnapshot.val().firstTime;
+    var trainFrequency = childSnapshot.val().trainFrequency;
+
+// First Time (pushed back 1 year to make sure it comes before current time)
+    var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+
+    //current time defined in military time
+    var currentTime = moment();
+    console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
+    
+    var diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+    // Time apart (remainder)
+    var tRemainder = diffTime % trainFrequency;
+
+    // Minute Until Train
+    var tMinutesTillTrain = trainFrequency - tRemainder;
+    console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+
+    // Next Train
+    var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+    console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+
 
     var $tr = $("<tr>");
     $tr
       .attr("train-key", childSnapshot.key)
-      .append(`<td>${name}</td>`)
-      .append(`<td>${destination}</td>`)
-      .append(`<td>${time}</td>`)
-      .append(`<td>${frequency}</td>`);
+      .append(`<td>${trainName}</td>`)
+      .append(`<td>${trainDestination}</td>`)
+      .append(`<td>${trainFrequency}</td>`)
+      .append(`<td>${nextTrain.format("hh:mm")}</td>`)
+      .append(`<td>${tMinutesTillTrain}</td>`);
 
     $("tbody#train-info").append($tr);
 
-  });
-});
+  })
+})
